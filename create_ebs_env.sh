9cdbc997f18431b3376edcffcb43ac2ad898458d
Moorthy <ramamoorthy.n@gmail.com>
2018-11-13 21:16:10 +0800

    deployer

--- create_ebs_env.sh
+++ create_ebs_env.sh
@@ -10,23 +10,23 @@ fail() {
     exit 1
 }
 
-export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=ap-southeast-1
 
 datetag=$(date +%Y%m%d%H%M)
 identifier=deployer$datetag
-mkdir -p tmp/$identifier
+mkdir -p /tmp/$identifier
 
 echo "Creating EBS application $identifier"
 
 # Find the ID of the default VPC
-aws ec2 describe-vpcs --filters Name=isDefault,Values=true > tmp/$identifier/defaultvpc.json || fail
-vpcid=$(jq -r '.Vpcs[0].VpcId' tmp/$identifier/defaultvpc.json)
+aws ec2 describe-vpcs --filters Name=isDefault,Values=true > /tmp/$identifier/defaultvpc.json || fail
+vpcid=$(jq -r '.Vpcs[0].VpcId' /tmp/$identifier/defaultvpc.json)
 echo "default vpc is $vpcid"
 
 # Create an elasticbeantalk application
 aws elasticbeanstalk create-application \
     --application-name $identifier \
-    --description "deployer $env $datetag" > tmp/$identifier/ebcreateapp.json || fail
+    --description "deployer $env $datetag" > /tmp/$identifier/ebcreateapp.json || fail
 echo "ElasticBeanTalk application created"
 
 # Get the name of the latest Docker solution stack
@@ -40,8 +40,8 @@ aws elasticbeanstalk create-environment \
     --description "deployer API environment" \
     --tags "Key=Owner,Value=$(whoami)" \
     --solution-stack-name "$dockerstack" \
-    --tier "Name=WebServer,Type=Standard,Version=''" > tmp/$identifier/ebcreateapienv.json || fail
-apieid=$(jq -r '.EnvironmentId' tmp/$identifier/ebcreateapienv.json)
+    --tier "Name=WebServer,Type=Standard,Version=''" > /tmp/$identifier/ebcreateapienv.json || fail
+apieid=$(jq -r '.EnvironmentId' /tmp/$identifier/ebcreateapienv.json)
 echo "API environment $apieid is being created"
 
 # Upload the application version
@@ -50,13 +50,13 @@ aws s3 cp app-version-deployer.json s3://$identifier/
 aws elasticbeanstalk create-application-version \
     --application-name "$identifier" \
     --version-label deployer-api \
-    --source-bundle "S3Bucket=$identifier,S3Key=app-version-deployer.json" > tmp/$identifier/appversion.json
+    --source-bundle "S3Bucket=$identifier,S3Key=app-version-deployer.json" > /tmp/$identifier/appversion.json
 
 # Wait for the environment to be ready (green)
 echo -n "waiting for environment"
 while true; do
-    aws elasticbeanstalk describe-environments --environment-id $apieid > tmp/$identifier/$apieid.json
-    health="$(jq -r '.Environments[0].Health' tmp/$identifier/$apieid.json)"
+    aws elasticbeanstalk describe-environments --environment-id $apieid > /tmp/$identifier/$apieid.json
+    health="$(jq -r '.Environments[0].Health' /tmp/$identifier/$apieid.json)"
     if [ "$health" == "Green" ]; then break; fi
     echo -n '.'
     sleep 10
@@ -67,7 +67,7 @@ echo
 aws elasticbeanstalk update-environment \
     --application-name $identifier \
     --environment-id $apieid \
-    --version-label deployer-api > tmp/$identifier/$apieid.json
+    --version-label deployer-api > /tmp/$identifier/$apieid.json
 
-url="$(jq -r '.CNAME' tmp/$identifier/$apieid.json)"
+url="$(jq -r '.CNAME' /tmp/$identifier/$apieid.json)"
 echo "Environment is being deployed. Public endpoint is http://$url"
